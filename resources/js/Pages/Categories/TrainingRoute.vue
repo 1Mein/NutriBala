d553deab8c7b18890a0c6fb8c884b9217ceddf4b
1Mein <119441676+1Mein@users.noreply.github.com>
2024-10-08 16:59:54 +0500

    v1

--- resources/js/Pages/Categories/TrainingRoute.vue
+++ resources/js/Pages/Categories/TrainingRoute.vue
@@ -31,26 +31,30 @@ const cardItems = [
 ]
 
 
-const itemExps = [{
-    'forId':'0',
-    'Exps':[{
-        'id':'0',
-        'title': 'Ротовая полость',
-        'content': 'Это начальный отдел как пищеварительной, так и дыхательной систем. С внутренней стороны ротовая полость покрыта слизистой оболочкой. В здоровом состоянии цвет слизистой оболочки полости рта может быть от красновато-розового до градаций коричневого или черного.'
-    },{
-        'id':'1',
-        'title': 'Желудок',
-        'content': 'Резервуар верхнего отдела пищеварительного тракта, образованный из гладкой мышечной ткани, в котором происходит частичное расщепление и разжижение пищи и регулярное ее продвижение в подходящих объемах (порциями) в тонкую кишку.'
-    }]
-},
+const itemExps = [
     {
-        'forId':'1',
-        'Exps':[{
-            'id':'0',
+        'forId': '0',
+        'Exps': [{
+            'id': '0',
+            'title': 'Ротовая полость',
+            'content': 'Это начальный отдел как пищеварительной, так и дыхательной систем. С внутренней стороны ротовая полость покрыта слизистой оболочкой. В здоровом состоянии цвет слизистой оболочки полости рта может быть от красновато-розового до градаций коричневого или черного.'
+        }, {
+            'id': '1',
+            'title': 'Желудок',
+            'content': 'Резервуар верхнего отдела пищеварительного ' +
+                'asdfasdf' +
+                '' +
+                'тракта, образованный из гладкой мышечной ткани, в котором происходит частичное расщепление и разжижение пищи и регулярное ее продвижение в подходящих объемах (порциями) в тонкую кишку.'
+        }]
+    },
+    {
+        'forId': '1',
+        'Exps': [{
+            'id': '0',
             'title': 'Ротовая полоqweqweсть',
             'content': 'Это начальный отqweqweдел как пищеварительной, так и дыхательной систем. С внутренней стороны ротовая полость покрыта слизистой оболочкой. В здоровом состоянии цвет слизистой оболочки полости рта может быть от красновато-розового до градаций коричневого или черного.'
-        },{
-            'id':'1',
+        }, {
+            'id': '1',
             'title': 'Желуqweдок',
             'content': 'Резервуаadasdр верхнего отдела пищеварительного тракта, образованный из гладкой мышечной ткани, в котором происходит частичное расщепление и разжижение пищи и регулярное ее продвижение в подходящих объемах (порциями) в тонкую кишку.'
         }]
@@ -134,33 +138,34 @@ function toggleAccordion(index) {
 
             <div v-if="currentItem" id="accordion">
                 <a @click="currentItem=null"
-                   class="lg:ms-[150px]  cursor-pointer underline text-[20px] font-semibold text-designColor1">← Назад</a><br>
-            <div class="lg:mx-[200px] mt-[20px]">
+                   class="lg:ms-[150px]  cursor-pointer underline text-[20px] font-semibold text-designColor1">←
+                    Назад</a><br>
+                <div class="lg:mx-[200px] mt-[20px]">
 
-
-                <div v-for="itemExp in itemExps[currentItem]['Exps']" class="border-b border-slate-200">
-                    <button @click="toggleAccordion(itemExp['id'])"
-                            class="w-full flex justify-between items-center py-5 text-slate-800 px-5
+                    <div v-for="itemExp in itemExps[currentItem]['Exps']" class="border-b border-slate-200">
+                        <button @click="toggleAccordion(itemExp['id'])"
+                                class="w-full flex justify-between items-center py-5 text-slate-800 px-5
                                 transition-bg duration-500 hover:bg-gray-200
                                     ">
-                        <span>{{itemExp['title']}}</span>
-                        <span :id="'icon-' + itemExp['id']" class="text-slate-800 transition-transform duration-300">
-                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                            <span>{{ itemExp['title'] }}</span>
+                            <span :id="'icon-' + itemExp['id']"
+                                  class="text-slate-800 transition-transform duration-300">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                               class="w-4 h-4">
                             <path fill-rule="evenodd"
                                   d="M11.78 9.78a.75.75 0 0 1-1.06 0L8 7.06 5.28 9.78a.75.75 0 0 1-1.06-1.06l3.25-3.25a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06Z"
                                   clip-rule="evenodd"/>
                           </svg>
                         </span>
-                    </button>
-                    <div :id="'content-'+itemExp['id']" class="max-h-0 overflow-hidden transition-all duration-300 ease-in-out">
-                        <div class="pb-5 text-sm text-slate-500">
-                            {{itemExp['content']}}
+                        </button>
+                        <div :id="'content-'+itemExp['id']"
+                             class="max-h-0 overflow-hidden transition-all duration-300 ease-in-out">
+                            <div class="pb-5 text-sm text-slate-500">
+                                <span v-html="itemExp['content']"></span>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-
-            </div>
             </div>
 
         </div>
